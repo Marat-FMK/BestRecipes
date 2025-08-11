161f128dd41199115c2da98872cdbf81e6e050db
marat_FMK <kosjachok8@yandex.ru>
2025-08-12 01:16:58 +0300

    add plan

--- BestRecipes/BestRecipes/NetworkManager/NetworkManager.swift
+++ BestRecipes/BestRecipes/NetworkManager/NetworkManager.swift
@@ -11,3 +11,13 @@ class NeworkManager {
     
     
 }
+
+// 793c4a9318a740b8af0b9f829165475d
+
+//Structs >>
+// details : name / image url / rating / revies Count / unstructions / ingredients / weightIngredients / time
+// creatorName / creatorImageURL
+
+// Fetch methods >>
+// search reciepe/ trending now / popular category / popular creators  ---> 30 Items ?!?
+
