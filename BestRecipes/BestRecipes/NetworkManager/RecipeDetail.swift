9bdb010ed9573d8ea5f25243da5c2c6e5f325b05
marat_FMK <kosjachok8@yandex.ru>
2025-08-14 14:26:48 +0300

    add recipe detail model

---
+++ BestRecipes/BestRecipes/NetworkManager/RecipeDetail.swift
@@ -0,0 +1,31 @@
+//
+//  RecipeDetail.swift
+//  BestRecipes
+//
+//  Created by Marat Fakhrizhanov on 14.08.2025.
+//
+
+import Foundation
+
+struct RecipeDetail: Codable {
+    let id: Int
+    let title: String
+    let image: String
+    let spoonacularScore: String
+    let instructions: String
+    let preparationMinutes: Int
+    let cookingMinutes: Int
+    let readyInMinutes: Int
+    let extendedIngredients: [ExtendedIngredient]
+    let vegetarian: Bool
+    let glutenFree: Bool
+    let servings: Int
+    
+}
+
+struct ExtendedIngredient: Codable {
+    let name: String
+    let consistency: String
+    let amount: String
+    let unit: String
+}
