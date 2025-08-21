006316aec03804854078957fd55ef1650b9f6f39
Sergey <ser.suvorov345@gmail.com>
2025-08-21 20:47:33 +0300

    fix mainVC

--- BestRecipes/BestRecipes/SceneDelegate.swift
+++ BestRecipes/BestRecipes/SceneDelegate.swift
@@ -14,7 +14,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        let mainVC = CreateRecipeViewController()
+        let mainVC = WelcomeViewController()
         let navController = UINavigationController(rootViewController: mainVC)
         
         window = UIWindow(windowScene: windowScene)
