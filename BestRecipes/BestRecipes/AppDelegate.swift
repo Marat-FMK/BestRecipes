fbd46711e535ea5acae714467700f48e9e8b1756
SuvorovSergeyS <162491607+SuvorovSergeyS@users.noreply.github.com>
2025-08-23 12:25:42 +0300

    Revert "add IQKeyboardManagerSwift"

--- BestRecipes/BestRecipes/AppDelegate.swift
+++ BestRecipes/BestRecipes/AppDelegate.swift
@@ -6,7 +6,6 @@
 //
 
 import UIKit
-import IQKeyboardManagerSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,9 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        let keyboardManager = IQKeyboardManager.shared
-        keyboardManager.isEnabled = true
-        keyboardManager.resignOnTouchOutside = true
+        // Override point for customization after application launch.
         return true
     }
 
