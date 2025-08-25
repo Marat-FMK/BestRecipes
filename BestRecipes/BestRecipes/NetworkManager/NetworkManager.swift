05f3e1e9eb8dff69609d9e37852f44013bad2a24
marat_FMK <kosjachok8@yandex.ru>
2025-08-25 21:05:45 +0300

    fix recipe count in NetworkManager

--- BestRecipes/BestRecipes/NetworkManager/NetworkManager.swift
+++ BestRecipes/BestRecipes/NetworkManager/NetworkManager.swift
@@ -26,7 +26,7 @@ class NetworkManager {
                       trend: Bool = false,
                       cuisine: WorldCuisines? = nil,
                       mealType: MealType? = nil,
-                      maxRecipeCount: String = "1",
+                      maxRecipeCount: String = "6",
                       completion: @escaping(Result<[SearchedRecipe],NetworkError>) -> Void) {
         
         var urlComponents = URLComponents()
